6964c1231960c1e24a8053d7e14dd3b4eb186fac
k.kuzmin <k.kuzmin@npo-echelon.ru>
2017-01-26 23:05:06 +0300

    fix / filename

--- image_resize.py
+++ image_resize.py
@@ -1,43 +1,51 @@
 import argparse
 from PIL import Image
+import ntpath
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
 
 def set_resized_filename(path_to_file, width, height):
-    namefile = (path_to_file.split('/')[-1])
+    namefile = path_leaf(path_to_file)
     part_filename = namefile.split('.')
     return '{}__{}x{}.{}'.format(part_filename[0], width, height, part_filename[-1])
 
 
-def resize_image(params):
+def calc_size(width, height, scale, old_width, old_height):
+    if scale:
+        return (old_width*scale, old_height*scale)
+    
+    default_scale = old_width/old_height
+    
+    if width and height:
+        if width/height != old_width/old_height:
+            print('Proportion of image are changed!')
+        return (width, height)
+    elif height:
+        return (round(height*default_scale), height)
+    else:
+        return (width, round(width*default_scale))    
 
-    im = Image.open(params['path'])
+
+def resize_image(parametrs):
+
+    im = Image.open(parametrs['path'])
 
     width, height = im.size
     default_scale = round(width/height, 2)
 
-    new_width = params['width']
-    new_height = params['height']
-    new_scale = params['scale']
+    new_width = parametrs['width']
+    new_height = parametrs['height']
+    new_scale = parametrs['scale']
 
-    if new_scale:
-        im = im.resize((
-            width*new_scale, height*new_scale))
+    im = im.resize(calc_size(new_width, new_height, new_scale, width, height))
+    if parametrs['output']:
+        im.save(parametrs['output'], 'JPEG')
     else:
-        if new_width and new_height:
-            if round(new_width/new_height, 2) != default_scale:
-                print('Proportion of image are changed!')
-            im = im.resize((new_width, new_height))
-        elif not new_width:
-            im = im.resize((
-                round(new_height*default_scale), new_height))
-        else:
-            im = im.resize((
-                new_width, round(new_width*default_scale)))
-    if params['output']:
-        im.save(params['output'], 'JPEG')
-    else:
-        im.save(set_resized_filename(
-            params['path'], im.size[0], im.size[1]), 'JPEG')
+        im.save(set_resized_filename(parametrs['path'], im.size[0], im.size[1]), 'JPEG')
 
 
 if __name__ == '__main__':
@@ -58,4 +66,5 @@ if __name__ == '__main__':
     if (args.scale and args.width and args.hieght):
         print ('Too many arguments, input "Scale" or "Hieght"/"Width"!')
         exit(1)
+    #vars делает из нот итерабл Namespace() словарь
     resize_image(vars(args))
