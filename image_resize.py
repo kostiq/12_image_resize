994d2005cbe8e54a02a8ac1e8629dfeb77f5e939
k.kuzmin <k.kuzmin@npo-echelon.ru>
2017-01-28 11:46:02 +0300

    fix png

--- image_resize.py
+++ image_resize.py
@@ -16,39 +16,43 @@ def set_resized_filename(path_to_file, width, height):
 
 def calc_size(width, height, scale, old_width, old_height):
     if scale:
-        return (old_width*scale, old_height*scale)
-    
+        return (round(old_width*scale), round(old_height*scale))
+
     default_scale = old_width/old_height
-    
+
     if width and height:
-        if width/height != old_width/old_height:
-            print('Proportion of image are changed!')
+        if new_width/new_height != width/height:
+            print_warning()
         return (width, height)
     elif height:
         return (round(height*default_scale), height)
     else:
-        return (width, round(width*default_scale))    
+        return (width, round(width*default_scale))
 
 
 def resize_image(parametrs):
 
-    im = Image.open(parametrs['path'])
+    im = Image.open(parametrs.path)
 
     width, height = im.size
-    default_scale = round(width/height, 2)
 
-    new_width = parametrs['width']
-    new_height = parametrs['height']
-    new_scale = parametrs['scale']
+    new_width = parametrs.width
+    new_height = parametrs.height
+    new_scale = parametrs.scale
 
     im = im.resize(calc_size(new_width, new_height, new_scale, width, height))
-    if parametrs['output']:
-        im.save(parametrs['output'], 'JPEG')
+    if parametrs.output:
+        im.convert('RGB').save(parametrs.output)
     else:
-        im.save(set_resized_filename(parametrs['path'], im.size[0], im.size[1]), 'JPEG')
+        im.convert('RGB').save(set_resized_filename(
+            parametrs.path, im.size[0], im.size[1]))
 
 
-if __name__ == '__main__':
+def print_warning():
+    print('Proportion of image are changed!')
+
+
+def parse_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--path', help="Path to image", required=True)
@@ -57,14 +61,16 @@ if __name__ == '__main__':
     parser.add_argument(
         '--height', help="Height of resized image (px)", type=int)
     parser.add_argument(
-        '--scale', help="Scale of resized image", type=int)
+        '--scale', help="Scale of resized image", type=float)
     parser.add_argument(
         '--output', help="Path to resized image")
+    return parser.parse_args()
 
-    args = parser.parse_args()
+if __name__ == '__main__':
+
+    args = parse_arg()
 
     if (args.scale and args.width and args.hieght):
         print ('Too many arguments, input "Scale" or "Hieght"/"Width"!')
         exit(1)
-    #vars делает из нот итерабл Namespace() словарь
-    resize_image(vars(args))
+    resize_image(args)
